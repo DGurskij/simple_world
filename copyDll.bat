a74dd2f86357e59e947e8da18f129e758c16112c
Denis Gurskij <denis.gurskij2012@yandex.by>
2021-10-23 22:43:05 +0300

    add bat, fix filters

---
+++ copyDll.bat
@@ -0,0 +1,27 @@
+@echo off
+
+IF "%1" EQU "" goto error
+
+@ECHO copy DLL to path: %1
+
+XCOPY simple_world\Release\simple_world.dll %1\release_x32\ /y
+XCOPY simple_world\Release\simple_world.lib %1\release_x32\ /y
+
+XCOPY simple_world\x64\Release\simple_world.dll %1\release_x64\ /y
+XCOPY simple_world\x64\Release\simple_world.lib %1\release_x64\ /y
+
+XCOPY simple_world\Debug\simple_world.dll %1\debug_x32\ /y
+XCOPY simple_world\Debug\simple_world.lib %1\debug_x32\ /y
+
+XCOPY simple_world\x64\Debug\simple_world.dll %1\debug_x64\ /y
+XCOPY simple_world\x64\Debug\simple_world.lib %1\debug_x64\ /y
+
+XCOPY simple_world\simple_world.h %1\include\ /y
+
+pause
+exit
+
+:error
+@ECHO Undefined path, send it to first argument
+pause
+exit
